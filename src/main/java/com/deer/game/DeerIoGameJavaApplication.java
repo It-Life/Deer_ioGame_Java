d379b9eac2b9b3f3e2855088a938c02975b7b55a
DESKTOP-KKV978P\Admin <652597991@qq.com>
2023-03-29 14:41:08 +0800

    创建java游戏服务器库

---
+++ src/main/java/com/deer/game/DeerIoGameJavaApplication.java
@@ -0,0 +1,13 @@
+package com.deer.game;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeerIoGameJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeerIoGameJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/deer/game/DeerIoGameJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deer.game;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeerIoGameJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
